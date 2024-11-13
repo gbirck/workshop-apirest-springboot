5ba5d811e55e0d719a392da4b1ad596a8d772e6f
Gabriel Birck <birckzin@gmail.com>
2024-11-13 15:03:42 -0300

    Adicionado insert de users

--- src/main/java/com/park/demo_park_api/services/UserService.java
+++ src/main/java/com/park/demo_park_api/services/UserService.java
@@ -1,6 +1,8 @@
 package com.park.demo_park_api.services;
 
+import com.park.demo_park_api.entities.User;
 import com.park.demo_park_api.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,9 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
--- src/main/java/com/park/demo_park_api/web/controller/UserController.java
+++ src/main/java/com/park/demo_park_api/web/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.park.demo_park_api.web.controller;
 
+import com.park.demo_park_api.entities.User;
 import com.park.demo_park_api.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        User userTest = userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userTest);
+    }
 }
