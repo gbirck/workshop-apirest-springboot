66d77b2aeff44a007c0824935adf53c18cee8805
Gabriel Birck <birckzin@gmail.com>
2024-11-19 14:01:08 -0300

    Adicionado controller de autenticação

---
+++ src/main/java/com/park/demo_park_api/web/controller/AuthenticationController.java
@@ -0,0 +1,47 @@
+package com.park.demo_park_api.web.controller;
+
+import com.park.demo_park_api.jwt.JwtToken;
+import com.park.demo_park_api.jwt.JwtUserDetailsService;
+import com.park.demo_park_api.web.dto.UserLoginDTO;
+import com.park.demo_park_api.web.exception.ErrorMessage;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+public class AuthenticationController {
+
+    private final JwtUserDetailsService jwtUserDetailsService;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/auth")
+    public ResponseEntity<?> authenticate(@RequestBody @Valid UserLoginDTO loginDto, HttpServletRequest request) {
+        log.info("Login authentication process {}", loginDto.getUsername());
+        try {
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+            authenticationManager.authenticate(authenticationToken);
+            JwtToken token = jwtUserDetailsService.getTokenAuthenticated(loginDto.getUsername());
+            return ResponseEntity.ok(token);
+        }
+        catch (AuthenticationException e) {
+            log.warn("Bad Credentials from username {}", loginDto.getUsername());
+        }
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Invalid Credentials"));
+    }
+}
---
+++ src/main/java/com/park/demo_park_api/web/dto/UserLoginDTO.java
@@ -0,0 +1,21 @@
+package com.park.demo_park_api.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserLoginDTO {
+
+    @NotBlank
+    @Email(message = "Invalid email format", regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
+    private String username;
+    @NotBlank
+    @Size(min = 6, max = 10)
+    private String password;
+}
